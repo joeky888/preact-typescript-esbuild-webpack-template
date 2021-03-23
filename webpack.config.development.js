353e9059369b6940f0e42b213e057c79fe0ed627
Joeky <joeky5888@gmail.com>
2021-03-23 13:18:07 +0800

    Add file-loader
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -57,6 +57,13 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.(png|jpe?g|gif|jp2|webp|glsl)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                },
+            },
         ],
     },
 
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -49,6 +49,13 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.(png|jpe?g|gif|jp2|webp|glsl)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                },
+            },
         ],
     },
 
